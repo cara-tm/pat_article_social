3d7a3088f543aa6d20b9cac1c0e0b47dbb936103
Patrick LEFEVRE <patrick.lefevre@gmail.com>
2015-09-29 11:12:37 +0200

    Update pat_article_social.php
    Use of more recent fb script with app id.

--- pat_article_social.php
+++ pat_article_social.php
@@ -395,7 +395,7 @@ function fb($atts)
 	if ( !gps('txpreview') ) {
 
 		if( preg_match('#^https:\/\/w{3}\.facebook\.com\/[a-z-A-Z-0-9.]*\/posts\/[0-9]*$#i', $url) ) {
-			return '<div id="fb-root"></div><script>!function(e,t,n){var c,o=e.getElementsByTagName(t)[0];e.getElementById(n)||(c=e.createElement(t),c.id=n,c.src="//connect.facebook.net/'._pat_locale($locale).'/all.js#xfbml=1",o.parentNode.insertBefore(c,o))}(document,"script","facebook-jssdk");</script><div class="fb-post" data-href="'.$url.'"></div>';
+			return '<div id="fb-root"></div><script>(function(d,s,id){var js,fjs=d.getElementsByTagName(s)[0];if(d.getElementById(id)) return;js=d.createElement(s);js.id=id;js.src="//connect.facebook.net/'._pat_locale($locale).'/sdk.js#xfbml=1&version=v2.4&appId=373162966153650";fjs.parentNode.insertBefore(js,fjs);}(document,'script','facebook-jssdk'));</script><div class="fb-post" data-href="'.$url.'"></div>';
 		}
 
 		return '';
